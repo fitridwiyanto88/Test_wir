54520b75dbf9e35569944a6c88f0c2d172f498ab
fitridwiyanto <fitridwiyanto88@gmail.com>
2020-03-18 14:03:48 +0700

    first commit

---
+++ oop.php
@@ -0,0 +1,16 @@
+<?php
+class Datas
+{
+ public function the_name()
+ {
+ echo 'Nama Saya FitriDwiyanto';
+ }
+
+ public function get_alamat ()
+{
+ return 'Klaten Jawa Tengah';
+ }
+}
+$belajar = new Datas();
+$belajar->the_name();
+?>
